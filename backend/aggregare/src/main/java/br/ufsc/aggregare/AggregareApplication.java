4906d1ce44209a52536132a9597388ba4b79c241
Beilfuss <matheusdicimb@gmail.com>
2025-08-24 14:28:39 -0300

    initial project structure

---
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/AggregareApplication.java
@@ -0,0 +1,13 @@
+package br.ufsc.aggregare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregareApplication.class, args);
+	}
+
+}
