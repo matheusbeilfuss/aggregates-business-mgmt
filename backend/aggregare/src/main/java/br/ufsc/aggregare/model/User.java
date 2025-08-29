b2436843768e597cef9e322309d68bbf7020728b
Beilfuss <matheusdicimb@gmail.com>
2025-08-29 10:26:55 -0300

    feat: equals, hashCode and Serializable

--- backend/aggregare/src/main/java/br/ufsc/aggregare/model/User.java
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/model/User.java
@@ -1,6 +1,8 @@
 package br.ufsc.aggregare.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,7 +12,8 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements Serializable {
+	@Serial private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -99,5 +102,16 @@ public class User {
 
 	public void setAdmin(Boolean admin) {
 		this.admin = admin;
+	}
+
+	@Override public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass())
+			return false;
+		User user = (User) o;
+		return Objects.equals(id, user.id);
+	}
+
+	@Override public int hashCode() {
+		return Objects.hashCode(id);
 	}
 }
