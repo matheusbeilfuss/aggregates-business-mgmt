da91f57917c42cdf7405ac081115b4b727d88f9a
Beilfuss <matheusdicimb@gmail.com>
2025-08-29 10:32:54 -0300

    feat: delete and update user

--- backend/aggregare/src/main/java/br/ufsc/aggregare/controller/UserController.java
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/controller/UserController.java
@@ -5,8 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +34,24 @@ public class UserController {
 		user = service.insert(user);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
 		return ResponseEntity.created(uri).body(user);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 
 	@GetMapping
--- backend/aggregare/src/main/java/br/ufsc/aggregare/service/UserService.java
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/service/UserService.java
@@ -1,21 +1,63 @@
 package br.ufsc.aggregare.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.ufsc.aggregare.model.User;
 import br.ufsc.aggregare.repository.UserRepository;
+import br.ufsc.aggregare.service.exception.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
 
+	private final UserRepository repository;
+
 	@Autowired
-	private UserRepository repository;
+	public UserService(UserRepository repository) {
+		this.repository = repository;
+	}
 
 	public User insert(User user) {
 		return repository.save(user);
+	}
+
+	public void delete(Long id) {
+		try {
+			User user = findById(id);
+			repository.delete(user);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public User update(Long id, User newUser) {
+		try {
+			User user = repository.getReferenceById(id);
+			updateData(user, newUser);
+			return repository.save(user);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public void updateData(User user, User newUser) {
+		user.setFirstName(newUser.getFirstName());
+		user.setLastName(newUser.getLastName());
+		user.setUsername(newUser.getUsername());
+		user.setEmail(newUser.getEmail());
+		user.setPassword(newUser.getPassword());
+		user.setImgUrl(newUser.getImgUrl());
+	}
+
+	public User findById(Long id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public List<User> findAll() {
