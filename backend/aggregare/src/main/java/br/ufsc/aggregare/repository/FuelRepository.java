73bb76c16b5d0ecd9937bb828d6f8f1a1c422117
Beilfuss <matheusdicimb@gmail.com>
2025-12-21 13:18:30 -0300

    feat: expense, fixed expense & fuel repositories

---
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/repository/FuelRepository.java
@@ -0,0 +1,9 @@
+package br.ufsc.aggregare.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufsc.aggregare.model.Fuel;
+
+public interface FuelRepository extends JpaRepository<Fuel, Long> {
+
+}
