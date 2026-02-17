3c119daf15ebc521fc11088d5eeb061c307ffac3
Beilfuss <matheusdicimb@gmail.com>
2026-02-17 20:17:52 -0300

    cr: eliminating double cors configuration

--- backend/aggregare/src/main/java/br/ufsc/aggregare/config/WebConfig.java
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/config/WebConfig.java
@@ -1,27 +1,15 @@
 package br.ufsc.aggregare.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Value("${api.cors.allowed-origins}")
-	private String allowedOrigins;
-
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/uploads/**")
 				.addResourceLocations("file:uploads/");
-	}
-
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOrigins(allowedOrigins.split(","))
-				.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
 	}
 }
