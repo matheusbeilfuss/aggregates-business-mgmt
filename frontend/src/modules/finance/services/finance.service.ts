ca7aac7f963f3b8678cb04209888f21b147f0ed3
Beilfuss <matheusdicimb@gmail.com>
2026-03-07 09:41:02 -0300

    cr: using toIsoDate in finance service

--- frontend/src/modules/finance/services/finance.service.ts
+++ frontend/src/modules/finance/services/finance.service.ts
@@ -1,17 +1,15 @@
 import { api } from "@/lib/api";
 import { Expense, Payment } from "../types";
-import { format } from "date-fns";
-
-const formatDate = (date: Date) => format(date, "yyyy-MM-dd");
+import { toIsoDate } from "@/utils";
 
 const getExpenses = (startDate: Date, endDate: Date) =>
   api.get<Expense[]>(
-    `/expenses?startDate=${formatDate(startDate)}&endDate=${formatDate(endDate)}`,
+    `/expenses?startDate=${toIsoDate(startDate)}&endDate=${toIsoDate(endDate)}`,
   );
 
 const getPayments = (startDate: Date, endDate: Date) =>
   api.get<Payment[]>(
-    `/payments?startDate=${formatDate(startDate)}&endDate=${formatDate(endDate)}`,
+    `/payments?startDate=${toIsoDate(startDate)}&endDate=${toIsoDate(endDate)}`,
   );
 
 export const financeService = { getExpenses, getPayments };
