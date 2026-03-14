72344f755c3709ba9db491253fc2722fbe331895
Beilfuss <matheusdicimb@gmail.com>
2026-03-14 18:46:40 -0300

    cr: date-fns -> toISODate

--- frontend/src/modules/receivables/services/receivable.service.ts
+++ frontend/src/modules/receivables/services/receivable.service.ts
@@ -1,14 +1,12 @@
 import { api } from "@/lib/api";
 import { Receivable } from "../types";
-import { format } from "date-fns";
-
-const formatDate = (date: Date) => format(date, "yyyy-MM-dd");
+import { toIsoDate } from "@/utils";
 
 export const receivableService = {
   getAll: (startDate?: Date, endDate?: Date) => {
     if (startDate && endDate) {
       return api.get<Receivable[]>(
-        `/orders/receivables?startDate=${formatDate(startDate)}&endDate=${formatDate(endDate)}`,
+        `/orders/receivables?startDate=${toIsoDate(startDate)}&endDate=${toIsoDate(endDate)}`,
       );
     }
     return api.get<Receivable[]>("/orders/receivables");
