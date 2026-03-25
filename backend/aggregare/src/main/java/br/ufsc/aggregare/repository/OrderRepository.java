6f8f8fccc9f0b6e6a9b039299824935a6bc4b0d3
Beilfuss <matheusdicimb@gmail.com>
2026-03-24 22:21:01 -0300

    cr: using order type enum in find product balance summary

--- backend/aggregare/src/main/java/br/ufsc/aggregare/repository/OrderRepository.java
+++ backend/aggregare/src/main/java/br/ufsc/aggregare/repository/OrderRepository.java
@@ -36,7 +36,7 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
        LEFT JOIN p.category c
        WHERE o.scheduledDate BETWEEN :startDate AND :endDate
        AND o.paymentStatus IN :statuses
-       AND o.type = 'MATERIAL'
+       AND o.type = br.ufsc.aggregare.model.enums.OrderTypeEnum.MATERIAL
        AND o.product IS NOT NULL
        GROUP BY p.id, p.name, c.name
        ORDER BY SUM(o.orderValue - o.remainingValue) DESC
