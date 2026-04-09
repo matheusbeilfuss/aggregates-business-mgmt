1097880feefacc01964bd9c29152c75c3d5bfd4a
Beilfuss <matheusdicimb@gmail.com>
2026-04-09 15:22:02 -0300

    cr: using payment status enum in order border color

--- frontend/src/modules/order/utils/getPaymentBorderColor.ts
+++ frontend/src/modules/order/utils/getPaymentBorderColor.ts
@@ -1,5 +1,12 @@
-export function getPaymentBorderColor(paymentStatus?: string): string {
-  if (paymentStatus === "PAID") return "#22c55e";
-  if (paymentStatus === "PARTIAL") return "#f59e0b";
-  return "var(--color-primary-40)";
+import { PaymentStatusEnum } from "@/types";
+
+export function getPaymentBorderColor(status: PaymentStatusEnum): string {
+  switch (status) {
+    case PaymentStatusEnum.PAID:
+      return "#22c55e";
+    case PaymentStatusEnum.PARTIAL:
+      return "#f59e0b";
+    case PaymentStatusEnum.PENDING:
+      return "var(--color-primary-40)";
+  }
 }
