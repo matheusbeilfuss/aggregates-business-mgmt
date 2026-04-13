7c6c9a1f7b6e470f40dced52829dd4fc3187ea9d
Beilfuss <matheusdicimb@gmail.com>
2026-04-13 19:58:40 -0300

    cr: set -e and avoiding traversing all files in uploads

--- backend/abm/entrypoint.sh
+++ backend/abm/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+set -e
 mkdir -p /app/uploads
-chown -R appuser:appuser /app/uploads
+if [ "$(stat -c '%U:%G' /app/uploads)" != "appuser:appuser" ]; then
+  chown appuser:appuser /app/uploads
+fi
 exec su -s /bin/sh appuser -c '/opt/java/openjdk/bin/java -jar /app/app.jar'
