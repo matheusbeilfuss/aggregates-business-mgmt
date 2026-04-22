de99a7fee9f78326ebc657cc83cea26c72f7f3e0
Beilfuss <matheusdicimb@gmail.com>
2026-04-22 06:51:37 -0300

    cr: address filling improved in order editing

--- frontend/src/modules/order/components/OrderForm.tsx
+++ frontend/src/modules/order/components/OrderForm.tsx
@@ -79,27 +79,36 @@ export function OrderForm({
 
   const quantityChangedByUser = useRef(!isEditing);
 
+  const prevClientId = useRef<number | undefined>(undefined);
+
   useEffect(() => {
     if (!client) return;
+
+    const isClientSwitch =
+      prevClientId.current !== undefined && prevClientId.current !== client.id;
+    prevClientId.current = client.id;
+
     form.setValue("clientName", client.name);
     form.setValue("cpfCnpj", formatCpfCnpj(client.cpfCnpj ?? ""));
 
-    if (client.address) {
-      form.setValue("cep", formatCep(client.address.cep ?? ""));
-      form.setValue("street", client.address.street);
-      form.setValue("number", client.address.number);
-      form.setValue("complement", client.address.complement ?? "");
-      form.setValue("neighborhood", client.address.neighborhood);
-      form.setValue("city", client.address.city);
-      form.setValue("state", client.address.state);
-    } else if (!isEditing) {
-      form.setValue("cep", "");
-      form.setValue("street", "");
-      form.setValue("number", "");
-      form.setValue("complement", "");
-      form.setValue("neighborhood", "");
-      form.setValue("city", "");
-      form.setValue("state", "");
+    if (!isEditing || isClientSwitch) {
+      if (client.address) {
+        form.setValue("cep", formatCep(client.address.cep ?? ""));
+        form.setValue("street", client.address.street);
+        form.setValue("number", client.address.number);
+        form.setValue("complement", client.address.complement ?? "");
+        form.setValue("neighborhood", client.address.neighborhood);
+        form.setValue("city", client.address.city);
+        form.setValue("state", client.address.state);
+      } else {
+        form.setValue("cep", "");
+        form.setValue("street", "");
+        form.setValue("number", "");
+        form.setValue("complement", "");
+        form.setValue("neighborhood", "");
+        form.setValue("city", "");
+        form.setValue("state", "");
+      }
     }
 
     if (client.phones?.length) {
