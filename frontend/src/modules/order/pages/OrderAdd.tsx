38b2354c47ff447613fe633fd9525c25611ae0c4
Beilfuss <matheusdicimb@gmail.com>
2026-04-22 06:51:02 -0300

    refactor: observations undefined when empty

--- frontend/src/modules/order/pages/OrderAdd.tsx
+++ frontend/src/modules/order/pages/OrderAdd.tsx
@@ -72,7 +72,7 @@ export function OrderAdd() {
         complement: data.complement || undefined,
         scheduledDate: data.scheduledDate,
         scheduledTime: data.scheduledTime,
-        observations: data.observations ?? null,
+        observations: data.observations || undefined,
         orderValue: data.orderValue!,
         productId: data.type === "MATERIAL" ? data.productId! : null,
         m3Quantity: data.type === "MATERIAL" ? data.m3Quantity : undefined,
