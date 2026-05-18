51329d718657868aee591cbd1f1cfc34653f2e31
Beilfuss <matheusdicimb@gmail.com>
2026-05-18 20:22:37 -0300

    refactor: updatind login password schema

--- frontend/src/modules/auth/schemas/login.schema.ts
+++ frontend/src/modules/auth/schemas/login.schema.ts
@@ -6,7 +6,7 @@ export const loginSchema = z.object({
     .min(5, "O usuário deve ter pelo menos 5 caracteres"),
   password: z
     .string({ required_error: "A senha é obrigatória" })
-    .min(5, "A senha deve ter pelo menos 5 caracteres"),
+    .min(8, "A senha deve ter pelo menos 8 caracteres"),
 });
 
 export type LoginSchema = z.infer<typeof loginSchema>;
