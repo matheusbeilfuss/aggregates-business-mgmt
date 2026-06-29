349c48861198313b47681ae2f8a914f9e9cc430e
Beilfuss <matheusdicimb@gmail.com>
2026-06-29 20:07:39 -0300

    fix: using loadEnv in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,36 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  define: {
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version ?? "0.0.0"),
-  },
-  server: {
-    allowedHosts:
-      process.env.VITE_ALLOWED_HOSTS === "all"
-        ? true
-        : ["localhost", "127.0.0.1", "::1"],
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-        rewrite: (reqPath) => reqPath.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    define: {
+      __APP_VERSION__: JSON.stringify(
+        process.env.npm_package_version ?? "0.0.0",
+      ),
+    },
+    server: {
+      allowedHosts:
+        env.VITE_ALLOWED_HOSTS === "all"
+          ? true
+          : ["localhost", "127.0.0.1", "::1"],
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+          rewrite: (reqPath) => reqPath.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
